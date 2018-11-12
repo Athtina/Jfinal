face1a7e730f0862ea8babf2ca092bd5dd37a48b
yanli.zhang <yanli.zhang@nio.com>
2018-11-12 17:03:25 +0800

    切面日志实现

--- src/main/java/com/example/common/annotation/SystemLog.java
+++ src/main/java/com/example/common/annotation/SystemLog.java
@@ -1,9 +1,20 @@
 package com.example.common.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @Author yanli.zhang
  * @dateTime 2018/11/8 17:04
  */
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface SystemLog {
   Type type() default Type.SEARCH;
 
